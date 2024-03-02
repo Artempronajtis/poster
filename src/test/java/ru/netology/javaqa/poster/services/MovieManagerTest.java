ef4bbb557303e415411a0d3c9cb34d7fe651c282
Artem test <artempronajtis@gmail.com>
2024-03-02 20:17:03 +0200

    test changes

--- src/test/java/ru/netology/javaqa/poster/services/MovieManagerTest.java
+++ src/test/java/ru/netology/javaqa/poster/services/MovieManagerTest.java
@@ -1,34 +1,58 @@
 package ru.netology.javaqa.poster.services;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MovieManagerTest {
+    @Test
+    public void testFindLastWhenLessThanLimit() {
+        MovieManager manager = new MovieManager(5);
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+
+        assertArrayEquals(new String[]{"Movie 2", "Movie 1"}, manager.findLast());
+    }
 
     @Test
-    public void testMovieManager() {
-
-        MovieManager manager = new MovieManager();
+    public void testFindLastWhenMoreThanLimit() {
+        MovieManager manager = new MovieManager(2);
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
         manager.addMovie("Movie 4");
-        manager.addMovie("Movie 5");
-        manager.addMovie("Movie 6");
-        manager.addMovie("Movie 7");
 
-        assertArrayEquals(new String[]{"Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5", "Movie 6", "Movie 7"}, manager.findAll());
-        assertArrayEquals(new String[]{"Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3"}, manager.findLast());
-
-
-        MovieManager limitedManager = new MovieManager(3);
-        limitedManager.addMovie("Movie A");
-        limitedManager.addMovie("Movie B");
-        limitedManager.addMovie("Movie C");
-        limitedManager.addMovie("Movie D");
-
-        assertArrayEquals(new String[]{"Movie A", "Movie B", "Movie C", "Movie D"}, limitedManager.findAll());
-        assertArrayEquals(new String[]{"Movie D", "Movie C", "Movie B"}, limitedManager.findLast());
+        assertArrayEquals(new String[]{"Movie 4", "Movie 3"}, manager.findLast());
     }
+
+    @Test
+    public void testFindLastWhenEqualToLimit() {
+        MovieManager manager = new MovieManager(3);
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+
+        assertArrayEquals(new String[]{"Movie 3", "Movie 2", "Movie 1"}, manager.findLast());
+    }
+
+    @Test
+    public void testAddMovie() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+
+        assertArrayEquals(new String[]{"Movie 1", "Movie 2"}, manager.findAll());
+    }
+
+    @Test
+    public void testFindAll() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+
+        assertArrayEquals(new String[]{"Movie 1", "Movie 2"}, manager.findAll());
+    }
+
+
 }
